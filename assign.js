f43f009db40d51142d0af8c732b6fb77694cc6fa
alidu11 <mugabejunior5@gmail.com>
2023-12-07 09:00:24 +0000

    first js commit

---
+++ assign.js
@@ -0,0 +1,36 @@
+alert("Hello There")
+
+// Assignment
+
+// for array
+let favourite_fruit = ["apple", "banana", "grapes"];
+console.log(favourite_fruit[0])
+console.log(favourite_fruit[1])
+console.log(favourite_fruit[2])
+
+
+// for boolean
+let java_script_fun = true;
+console.log(true)
+
+// for sum
+let num1 = 15;
+let num2 = 9;
+let sum = num1 + num2;
+console.log(sum)
+
+// for swap
+let a = 5
+let b = 10
+
+
+// for name
+let first_name = "Mohammed"
+let last_name = "Alidu"
+let full_name = last_name + first_name
+
+// for area
+const PI = 3.142
+const radius = 8
+let A = PI * radius * radius
+console.log(A)
